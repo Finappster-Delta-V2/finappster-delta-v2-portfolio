7f0f7681dad88dd3a02c2b0762d5ba042be42931
angeles-renjo <angeles.renjo@gmail.com>
2023-05-29 23:54:11 +1200

    document page

--- src/pages/Documents.jsx
+++ src/pages/Documents.jsx
@@ -1,12 +1,71 @@
-import React from 'react'
+import React from "react";
+import { AiOutlineFilePdf } from "react-icons/ai";
 
 function Documents() {
-  return (
-    <div className='section' data-anchor='page9'>
+  const documents = [
+    {
+      title: "Gantt Chart",
+      link: "https://drive.google.com/file/d/1xTwWa8VyFQeePMDtQprUUuv_hDi8AZPh/view?usp=drive_link",
+    },
+    {
+      title: "Burndown Chart",
+      link: "https://drive.google.com/drive/folders/17ahvIKMK2kXRtR9e9es-7OBo3X4kK0MB?usp=drive_link",
+    },
+    { title: "Change Request", link: "https://example.com/document3.pdf" },
+    { title: "Issue Form", link: "https://example.com/document4.pdf" },
+    { title: "Issue Log", link: "https://example.com/document5.pdf" },
+    { title: "WBS", link: "https://example.com/document6.pdf" },
+    { title: "Trello Board", link: "https://example.com/document7.pdf" },
+    {
+      title: "Zeta Investigation Report",
+      link: "https://example.com/document8.pdf",
+    },
+  ];
 
-      <div className='title'>Documents</div>
+  const handleFileClick = (link) => {
+    const linkElement = document.createElement("a");
+    linkElement.href = link;
+    linkElement.target = "_blank"; // Open the link in a new tab/window
+    linkElement.download = true; // Add the download attribute
+    linkElement.click();
+  };
+
+  const rows = Math.ceil(documents.length / 3); // Calculate the number of rows needed based on the number of documents
+
+  return (
+    <div className="section" data-anchor="page9">
+      <h1 className="mb-4 flex justify-center text-8xl text-gray-50">
+        Documents
+      </h1>
+      <div className="container mx-auto">
+        <table className="w-full table-auto">
+          <tbody>
+            {Array.from({ length: rows }).map((_, rowIndex) => (
+              <tr key={rowIndex}>
+                {Array.from({ length: 3 }).map((_, colIndex) => {
+                  const index = rowIndex * 3 + colIndex;
+                  if (index >= documents.length) return null; // Skip empty cells for documents that don't exist
+                  const document = documents[index];
+                  return (
+                    <td
+                      key={index}
+                      className="cursor-pointer rounded border-4 p-4 text-center"
+                      onClick={() => handleFileClick(document.link)}
+                    >
+                      <div className="flex flex-col items-center">
+                        <AiOutlineFilePdf className="mb-2 text-8xl" />
+                        <span className="text-white">{document.title}</span>
+                      </div>
+                    </td>
+                  );
+                })}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Documents
+export default Documents;
--- src/pages/Poster.jsx
+++ src/pages/Poster.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React from "react";
 
 function Poster() {
-    return (
-    <div className='section' data-anchor ='page6'>
+  const openFile = () => {
+    // Replace 'your_file_url.pdf' with the actual file URL you want to link to
+    window.open("https://google.com", "_blank");
+  };
 
-        <div className='title'>Poster</div>
-        </div>
-    )
+  return (
+    <div className="section" data-anchor="page6" style={{ overflow: "hidden" }}>
+      <div
+        className="flex h-full flex-col content-center items-center justify-center"
+        onClick={openFile}
+        style={{ cursor: "pointer" }}
+      >
+        <img
+          src="https://images.unsplash.com/photo-1684827844157-5a9085408044?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+          alt="Poster"
+          style={{ width: "25%", height: "auto" }}
+        />
+      </div>
+    </div>
+  );
 }
 
-export default Poster
+export default Poster;
